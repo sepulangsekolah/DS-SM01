82db7da2c3ae351b51324269d34513995fd732bc
Laymoth <laymoth@pm.me>
2021-10-11 00:27:10 +0700

    Linked list get

---
+++ LLGet.java
@@ -0,0 +1,20 @@
+
+// Java code to demonstrate the working
+// of get(int index) in linked list
+import java.util.LinkedList;
+
+public class LLGet {
+    public static void main(String[] args) {
+        // declaring a LinkedList
+        LinkedList<String> list = new LinkedList<String>();
+        // adding elements using add()
+        list.add("Linked");
+        list.add("2");
+        list.add("List");
+        list.add("5");
+        // printing the whole list
+        System.out.println("The elements in List are : " + list);
+        // using get() to print element at index 3
+        System.out.println("Element at index 3 is : " + list.get(3));
+    }
+}
