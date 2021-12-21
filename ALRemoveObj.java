1eaf298ba23725d7dd229e2b599d38fbce6f5627
Laymoth <laymoth@pm.me>
2021-12-22 00:27:17 +0700

    Create ALRemoveObj.java

---
+++ ALRemoveObj.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class ALRemoveObj {
+    public static void main(String[] args) {
+        List<Integer> al = new ArrayList<>();
+        al.add(15);
+        al.add(30);
+        al.add(40);
+        al.add(55);
+        al.add(70);
+        System.out.println("Original ArrayList : " + al);
+        al.remove(new Integer(40));
+        System.out.println("Modified ArrayList : " + al);
+    }
+}
